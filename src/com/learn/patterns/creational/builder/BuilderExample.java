48ce807cece2b4175490113dc7e508ae210b4ae4
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2019-05-14 15:33:54 +0300

    Builder pattern: minor changes
    Import static classes

--- src/com/learn/patterns/creational/builder/BuilderExample.java
+++ src/com/learn/patterns/creational/builder/BuilderExample.java
@@ -4,19 +4,22 @@ import com.learn.patterns.creational.builder.cars.Car;
 import com.learn.patterns.creational.builder.cars.Manual;
 import com.learn.patterns.creational.builder.director.CarDirector;
 
+import static com.learn.patterns.creational.builder.cars.Car.CarBuilder;
+import static com.learn.patterns.creational.builder.cars.Manual.ManualBuilder;
+
 public class BuilderExample {
 
     public static void main(String[] args) {
         CarDirector carDirector = new CarDirector();
 
-        Car.CarBuilder carBuilder = new Car.CarBuilder();
+        CarBuilder carBuilder = new CarBuilder();
         carDirector.constructSportsCar(carBuilder);
         Car car = carBuilder.build();
         System.out.println("Cat build: " + car.getType());
 
         System.out.println();
 
-        Manual.ManualBuilder manualBuilder = new Manual.ManualBuilder();
+        ManualBuilder manualBuilder = new ManualBuilder();
         carDirector.constructSportsCar(manualBuilder);
         Manual carManual = manualBuilder.build();
         System.out.println("Car manual built:\n" + carManual.print());
