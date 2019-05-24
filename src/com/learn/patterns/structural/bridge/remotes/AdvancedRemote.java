1a267cb41018401cef2a2883f79a8a7e57b29aab
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2019-05-24 12:27:50 +0300

    Bridge pattern: done

---
+++ src/com/learn/patterns/structural/bridge/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package com.learn.patterns.structural.bridge.remotes;
+
+import com.learn.patterns.structural.bridge.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super(device);
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
