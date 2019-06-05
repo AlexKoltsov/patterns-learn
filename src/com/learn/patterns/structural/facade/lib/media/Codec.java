83ce94e08dd1ae5077bdb8948b3e350255badf6b
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2019-06-05 14:39:41 +0300

    Facade pattern: done

---
+++ src/com/learn/patterns/structural/facade/lib/media/Codec.java
@@ -0,0 +1,4 @@
+package com.learn.patterns.structural.facade.lib.media;
+
+public interface Codec {
+}
---
+++ src/com/learn/patterns/structural/facade/lib/media/OggCompressionCodec.java
@@ -0,0 +1,7 @@
+package com.learn.patterns.structural.facade.lib.media;
+
+public class OggCompressionCodec implements Codec {
+
+    public String type = "ogg";
+
+}
